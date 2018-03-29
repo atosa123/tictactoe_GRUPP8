fccddf0e72f35c140b6a8939dc202527a3307b46
walseb <greatestmailofalltime@gmail.com>
2018-03-29 18:04:07 +0200

    Skapade meny samt logik till menyn

---
+++ Spelet/Assets/Scripts/Menu.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public void LoadMainScene()
+    {
+	SceneManager.LoadScene("Main");
+
+    }
+    public void ExitGame()
+    {
+	Application.Quit();
+	Debug.Log("Om programmet hadde byggds skulle det stängas ner nu");
+    }
+}
