5f8196d45f6c84a9bc8d02b5eab1a55f49551718
Luminixita <37596503+Luminixita@users.noreply.github.com>
2018-04-01 14:40:42 +0200

    Fixat vinst funktion

--- Spelet/Assets/Scripts/GameController.cs
+++ Spelet/Assets/Scripts/GameController.cs
@@ -78,35 +78,35 @@ public class GameController : MonoBehaviour {
 	{
 		moveCount++;
 
-		if (buttonList [0].text == playerSide && buttonList [1].text == playerSide && buttonList [2].text == playerSide)
+		if (buttonList [0].text == playerSide && buttonList [5].text == playerSide && buttonList [6].text == playerSide)
 		{
 			GameOver(playerSide);
 		} 
-		else if (buttonList [3].text == playerSide && buttonList [4].text == playerSide && buttonList [5].text == playerSide)
+		else if (buttonList [1].text == playerSide && buttonList [2].text == playerSide && buttonList [3].text == playerSide)
 		{
 			GameOver(playerSide);
 		} 
-		else if (buttonList [6].text == playerSide && buttonList [7].text == playerSide && buttonList [8].text == playerSide)
+		else if (buttonList [4].text == playerSide && buttonList [7].text == playerSide && buttonList [8].text == playerSide)
 		{
 			GameOver(playerSide);
 		} 
-		else if (buttonList [0].text == playerSide && buttonList [3].text == playerSide && buttonList [6].text == playerSide)
+		else if (buttonList [0].text == playerSide && buttonList [2].text == playerSide && buttonList [8].text == playerSide)
 		{
 			GameOver(playerSide);
 		} 
-		else if (buttonList [1].text == playerSide && buttonList [4].text == playerSide && buttonList [7].text == playerSide)
+		else if (buttonList [7].text == playerSide && buttonList [2].text == playerSide && buttonList [6].text == playerSide)
 		{
 			GameOver(playerSide);
 		} 
-		else if (buttonList [2].text == playerSide && buttonList [5].text == playerSide && buttonList [8].text == playerSide)
+		else if (buttonList [0].text == playerSide && buttonList [1].text == playerSide && buttonList [7].text == playerSide)
 		{
 			GameOver(playerSide);
 		} 
-		else if (buttonList [0].text == playerSide && buttonList [4].text == playerSide && buttonList [8].text == playerSide)
+		else if (buttonList [5].text == playerSide && buttonList [2].text == playerSide && buttonList [4].text == playerSide)
 		{
 			GameOver(playerSide);
 		} 
-		else if (buttonList [2].text == playerSide && buttonList [4].text == playerSide && buttonList [6].text == playerSide)
+		else if (buttonList [6].text == playerSide && buttonList [3].text == playerSide && buttonList [8].text == playerSide)
 		{
 			GameOver(playerSide);
 		} 
